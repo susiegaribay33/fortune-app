69c7d7fd288bf7de8a3801a9997b10fd2bb7aa8c
SusieG <susiegaribay33@gmail.com>
2021-10-10 11:38:14 -0500

    add random fortune and lotto numbers features

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,18 @@
 class MyExamplesController < ApplicationController
+  def random_fortune
+    fortunes = ["Money is coming to you soon!", "You will receive bad news tomorrow.", "Nothing exciting will happen to you for the next month."]
+    fortunes.shuffle!
+    render json: {your_fortune: "#{fortunes[0]}"}
+  end
+
+  def lotto_numbers
+    number = Random.new
+    lotto_numbers =[]
+    6.times do
+      lotto_numbers << number.rand(60)
+    end
+    render json: {lotto_numbers: "#{lotto_numbers}"}
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/random_fortune", controller: "my_examples", action: "random_fortune"
+  get "/lotto_numbers", controller: "my_examples", action: "lotto_numbers"
 end
